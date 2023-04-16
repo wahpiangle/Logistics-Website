17f4b8c437814f51f7779f49406af810d2b7b585
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-04-16 23:54:42 +0800

    update pricing page

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,6 +1,8 @@
 import { shipServices } from "../assets";
 import NavBar from "./NavBar";
 import { styles } from "../styles";
+import { pricing } from "./constants/pricing";
+
 export default function Pricing() {
     return(
         <>
@@ -12,11 +14,39 @@ export default function Pricing() {
                 </div>
             </div>
             <div>
-                <div className="flex items-center justify-center p-32 flex-col">
+                <div className="flex items-center justify-center p-10 lg:p-24 flex-col">
                     <div className="bg-[#00000012] text-black border-l-2 border-primaryyellow px-2 text-[12px] max-w-fit">Pricing</div>
                     <h1 className="xl:text-[30px] text-[20px] font-semibold">Our Best Pricing</h1>
-                    <div>
-
+                    <div className="grid lg:grid-cols-3 mt-6 justify-center text-center gap-8">
+                        {pricing.map((item, index) =>{
+                            if (index % 2 === 0){
+                                return(
+                                    <div className="bg-[#F4F4F4] px-12 py-8" key={item.index}>
+                                        <h2 className="text-md font-semibold">{item.title}</h2>
+                                        <h1 className="text-[46px] font-semibold whitespace-nowrap">${item.pricePerMonth}<span className="font-normal text-[12px]">/ month</span></h1>
+                                        <div className="mt-4">
+                                            {item.features.map((feature,index)=>(
+                                                <p key={index} className="py-4 border-t-[1px] border-[#D6D6D6]">{feature}</p>
+                                            ))}
+                                        <button className="bg-primary px-4 py-2 mt-2 text-white">Choose Plan</button>
+                                        </div>
+                                    </div>
+                                )
+                            }else{
+                                return(
+                                    <div className="bg-primary text-white px-12 py-8" key={item.index}>
+                                        <h2 className="text-md font-semibold">{item.title}</h2>
+                                        <h1 className="text-[46px] font-semibold whitespace-nowrap">${item.pricePerMonth}<span className="font-normal text-[12px]">/ month</span></h1>
+                                        <div className="mt-4">
+                                            {item.features.map((feature,index)=>(
+                                                <p key={index} className="py-4 border-t-[1px] border-[#D6D6D6]">{feature}</p>
+                                            ))}
+                                        </div>
+                                        <button className="yellow-gradient px-4 py-2 mt-2 text-black">Choose Plan</button>
+                                    </div>
+                                )
+                            }
+                        })}
                     </div>
                 </div>
             </div>
---
+++ src/components/constants/pricing.js
@@ -0,0 +1,35 @@
+export const pricing =[
+    {
+        id: 1,
+        title: 'Basic',
+        pricePerMonth: 39,
+        features: [
+            'Single Truck',
+            'Full Insurance',
+            '500 km per month',
+            'Real Time Tracking',
+        ],
+    },
+    {
+        id: 2,
+        title: 'Standard',
+        pricePerMonth: 59,
+        features: [
+            '3 Trucks',
+            'Full Insurance',
+            '1000 km per month',
+            'Real Time Tracking',
+        ],
+    },
+    {
+        id: 3,
+        title: 'Premium',
+        pricePerMonth: 99,
+        features: [
+            'Unlimited Trucks',
+            'Full Insurance',
+            'Unlimited km per month',
+            'Real Time Tracking',
+        ],
+    }
+]
